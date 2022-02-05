82ffc675bdc548810c03c5ab7902eb6609c40dc6
aneesh.damle <aneesh.damle@gmail.com>
2022-02-05 17:07:15 +0530

    Added basic map marker for API testing

---
+++ index.js
@@ -0,0 +1,19 @@
+// Initialize and add the map
+
+function initMap()
+{
+    // Location
+    const coep = { lat : 18.5293, lng : 73.85644 };
+    // Map centred at coep
+    const map = new google.maps.Map(document.getElementById("map"),
+        {
+            zoom: 5,
+            center: coep,
+        });
+    
+    // Marker, if needed, positioned at coep
+    const marker = new google.maps.Marker({
+        position: coep,
+        map: map,
+    });
+}
