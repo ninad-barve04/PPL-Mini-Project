a85398bebd1e11fcc26f21478874dfdd14c6029a
aneesh.damle <aneesh.damle@gmail.com>
2022-02-08 17:39:15 +0530

    Added Get latlong + zoom

--- index.js
+++ index.js
@@ -7,11 +7,39 @@ function initMap()
 
     // Map centred at coep
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 5,
+        zoom: 15,
         center: coep,
         mapTypeId: 'satellite'
     });
-    
+
+    // TODO : InfoWindow data will be fetched later
+    // Initial InfoWindow
+    let infoWindow = new google.maps.InfoWindow(
+    {
+            content: "Click on any place to get local % vegetation",
+            position: coep,
+    });
+
+    // Get LatLong
+    infoWindow.open(map);
+    // Detect mouseclick
+    map.addListener("click", (mapsMouseEvent) =>
+    {
+    	// Close the current InfoWindow
+    	infoWindow.close();
+    	
+    	// New InfoWindow
+    	infoWindow = new google.maps.InfoWindow({
+                position: mapsMouseEvent.latLng,
+        });
+        // New Window Content - LatLong + Zoom
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+            + "Zoom: " + map.getZoom()
+        );
+        infoWindow.open(map);
+    });
+
     // Marker, if needed, positioned at coep
     const marker = new google.maps.Marker({
         position: coep,
